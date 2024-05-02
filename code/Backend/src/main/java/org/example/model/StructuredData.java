393d097dab9d7516f2c3ac2bd57d8744a11bf717
julian <julian_2002@gmx.net>
2024-05-02 13:01:43 +0200

    added backend

--- code/Backend/src/main/java/org/example/model/StructuredData.java
+++
@@ -1,4 +0,0 @@
-package org.example.model;
-
-public record StructuredData(String salutation, String title, String gender, String firstName, String lastName) {
-}
--- code/Backend/src/main/java/org/example/parser/IParser.java
+++
@@ -1,7 +0,0 @@
-package org.example.parser;
-
-import org.example.model.StructuredData;
-
-public interface IParser{
-    StructuredData parse(String input);
-}
